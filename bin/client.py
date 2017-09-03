3e19480694bb28f541894894ab6ef9659caa873a
Tim <tdh911@gmail.com>
2017-09-03 16:38:00 -0500

    Added functionality to connect to server
    The client can pass all necessary information as parameters for the
    server to read and interpret.

--- bin/client.py
+++ bin/client.py
@@ -1,7 +1,31 @@
 import parser
+import urllib
+import urllib2 as url
+
+DEFAULT_ENDPOINT = "http://localhost:8080"
 
 def main():
     args = parser.get_args()
+
+    our_file = open(args.filepath,'r')
+    # TODO: this is dangerous if the file is TOO large
+    file_contents = our_file.read()
+    our_file.close()
+
+    endpoint = args.endpoint if args.endpoint else DEFAULT_ENDPOINT
+    request = url.Request(endpoint)
+    http_args = {
+            "method":args.mode,
+            "decentralized-db-user":args.user,
+            "secret-key":args.secret_key,
+            "filename":args.filename,
+            "file":file_contents
+        }
+    data = urllib.urlencode(http_args)
+
+    resp = url.urlopen(request, data)
+    out = resp.read()
+    print out 
 
 if __name__ == "__main__":
     main()
--- bin/parser.py
+++ bin/parser.py
@@ -4,7 +4,7 @@ CHOICES = ["list-all", "write", "read", "delete"]
 def get_parser():
     parser = argparse.ArgumentParser(description="Make call to decentralized db")
 
-    parser.add_argument("mode", choices=CHOICES)
+    parser.add_argument("method", choices=CHOICES)
 
     parser.add_argument("--user", "--public-key", dest="user", help="The public key associated with a user")
     parser.add_argument("--endpoint", dest="endpoint", default="http://localhost:8080", help="Where decentralized db is running")
