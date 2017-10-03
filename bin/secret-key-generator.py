865aba21a1dafa0110d5100faaef4883e3bc1146
Tim <tdh911@gmail.com>
2017-10-03 14:13:47 -0500

    Updated secret key generator so it no longer has to print 16 bytes exactly

--- bin/secret-key-generator.py
+++ bin/secret-key-generator.py
@@ -1,8 +1,9 @@
-from random import choice
-from string import digits, ascii_uppercase
+from random import choice, randint
+from string import digits, ascii_uppercase, ascii_lowercase
 
 def main():
-    print "".join(choice(digits + ascii_uppercase) for i in range(16))    
+    length = randint(15, 25)
+    print "".join(choice(digits + ascii_uppercase + ascii_lowercase) for i in range(length))    
 
 if __name__ == "__main__":
     main()
