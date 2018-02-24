1132b4109ce3a3917438c4cfcbeb32b7e07ad0e4
tdh911 <tdh911@gmail.com>
2018-02-23 21:39:10 -0600

    Update parser.py

--- bin/parser.py
+++ bin/parser.py
@@ -2,12 +2,12 @@ import argparse
 
 CHOICES = ["list-all", "write", "read", "delete"]
 def get_parser():
-    parser = argparse.ArgumentParser(description="Make call to decentralized db")
+    parser = argparse.ArgumentParser(description="Make call to distributed db")
 
     parser.add_argument("method", choices=CHOICES)
 
     parser.add_argument("--user", "--public-key", dest="user", help="The public key associated with a user")
-    parser.add_argument("--endpoint", dest="endpoint", default="http://localhost:8080", help="Where decentralized db is running")
+    parser.add_argument("--endpoint", dest="endpoint", default="http://localhost:8080", help="Where distributed db is running")
     
     parser.add_argument("--filepath", dest="filepath", help="The path to the file you want to write")
     parser.add_argument("--filename", dest="filename", help="The file you want to read")
